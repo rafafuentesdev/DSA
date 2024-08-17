34fbbd7ccdbe368d62684040cb26382ce32e8959
rafafuentesdev <github@rafafuentesdev.com>
2024-08-17 17:32:57 +0200

    Add pop method

--- code/01- Linked List/linked_list.py
+++ code/01- Linked List/linked_list.py
@@ -15,7 +15,7 @@ class LinkedList:
         new_node = Node(value)
 
         # If LinkedList is empty
-        if self.head is None:
+        if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
@@ -23,6 +23,22 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
     def prepend(self, value):
         pass
@@ -37,5 +53,6 @@ class LinkedList:
             temp = temp.next
 
 
-my_linked_list = LinkedList(4)
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
 my_linked_list.print_list()
